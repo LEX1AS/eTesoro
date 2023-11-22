e12dadf52d0123acc8b30641e8a017f3141f1b17
Elias Castellano Szczedrin <eliaxx.1811@gmail.com>
2023-11-22 19:05:53 +0000

    nav bar added

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,20 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+document.addEventListener("mousemove", (event) => {
+
+    var sidebarVisiblePercentage = (event.clientX / window.innerWidth) * 100;
+
+    if (sidebarVisiblePercentage < 10) {
+        document.getElementById("sidebar").style.left = "0";
+        document.getElementById("content").style.marginLeft = "250px";
+    }
+    else if (sidebarVisiblePercentage > 20) {
+                document.getElementById("sidebar").style.left = "-250px";
+        document.getElementById("content").style.marginLeft = "0";
+    }
+})
+//     } else {
+//         document.getElementById("sidebar").style.left = "-250px";
+//         document.getElementById("content").style.marginLeft = "0";
+//     }
+// });
